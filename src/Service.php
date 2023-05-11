49603777cea601b9255f06aefd8c93f268d46fbf
刘小飞 <liuxf@zhigujinyun.com>
2023-05-11 15:04:13 +0800

    initialize

---
+++ src/Service.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Shophy\Tclexiang;
+
+class Service
+{
+}
---
+++ src/exceptions/ArgumentException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Shophy\Tclexiang\Exception;
+
+class ArgumentException extends \Exception
+{
+    public function __construct($message, $paramName, $code = 0, Exception $previous = null) {
+        parent::__construct("{$paramName} {$message}", $code, $previous);
+    }
+
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
